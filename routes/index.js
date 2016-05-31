fc496422b3a829ed9566d98a6a980f7ec0d40b30
Waleed Zafar <waleedzaf94@gmail.com>
2016-05-31 20:25:57 +0800

    Use layouts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', function(req, res){
+	res.render('One', {});
+})
+
+router.get('/One.html', function(req, res){
+	res.render('One', {});
+})
+
+router.get('/Two.html', function(req, res){
+	res.render('Two', {});
+})
+
+router.get('/Three.html', function(req, res){
+	res.render('Three', {});
+})
 
 module.exports = router;
